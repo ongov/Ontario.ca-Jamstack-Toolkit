546ce0003db20e8fd703a566eb78b4d8ca38eb50
Alan Harnum <alan.harnum@ontario.ca>
2021-10-26 18:57:05 -0400

    feat: add Ontario Design System via @import in style.css, add style.css to <head>
    JIRA-Ref: OSI-30

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets/vendor")
+    eleventyConfig.addPassthroughCopy("src/assets")
 
     // Return your Object options:
     return {
