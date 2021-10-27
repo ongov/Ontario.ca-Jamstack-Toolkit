95bccda5ca56aa5420591b322b888ff5c66e8420
Alan Harnum <alan.harnum@ontario.ca>
2021-10-26 21:26:43 -0400

    fix: testing hooks
    JIRA-Ref: OSI-29

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var fs= require('fs');
 describe('Site generation', function() {
   describe('root index.html present', function() {
     it('should generate a root index.html', function() {
-      assert(fs.existsSync("dist/index.html"))
+      assert(fs.existsSync("dist/indexer.html"))
     });
   });
 });
