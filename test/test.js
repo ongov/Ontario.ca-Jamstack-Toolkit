d0667ec2d4e499762b9abffd16822525d52ee62f
Alan Harnum <alan.harnum@ontario.ca>
2021-10-28 11:05:24 -0400

    feat: additional test for successful design system copy on expected # of copied files.
    JIRA-Ref: OSI-30

--- test/test.js
+++ test/test.js
@@ -1,20 +1,24 @@
 const assert = require('assert');
 const fs= require('fs');
 
+const odsDir = "dist/assets/vendor/ontario-design-system";
+
 describe('Site generation', function() {
   describe('root index.html present', function() {
     it('should generate a root index.html', function() {
-      assert(fs.existsSync("dist/index.html"))
+      assert(fs.existsSync("dist/index.html"));
     });
   });
-  describe('Ontario design system folder present', function() {
+  describe('Ontario design system inclusion', function() {
     it('should copy over the design system assets', function() {
-      assert(fs.existsSync("dist/assets/vendor/ontario-design-system"))
+      assert(fs.existsSync(odsDir), "Expected directory for design system not found");
+      const length = fs.readdirSync(odsDir).length;
+      assert(fs.readdirSync(odsDir).length === 9, "Expected number of files in design system directory not found");
     });
   });
   describe('Site CSS file present', function() {
     it('Should copy over the site CSS file', function() {
-      assert(fs.existsSync("dist/assets/css/style.css"))
+      assert(fs.existsSync("dist/assets/css/style.css"));
     });
   });
 });
