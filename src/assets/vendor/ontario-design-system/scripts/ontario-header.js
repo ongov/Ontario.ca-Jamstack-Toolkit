0be3d35996767083ddfda447a4c23761d9915229
Alan Harnum <alan.harnum@ontario.ca>
2022-05-06 12:14:54 -0400

    chore: design system bumping and fixes.
    JIRA-Ref: OSI-152

--- src/assets/vendor/ontario-design-system/scripts/ontario-header.js
+++ src/assets/vendor/ontario-design-system/scripts/ontario-header.js
@@ -1,8 +1,6 @@
 // run asynchronously
 function deferInFn(fn) {
-    if (typeof fn === "function") {
-        setTimeout(fn, 0);
-    }
+    if (typeof fn === "function") setTimeout(fn, 0);
 }
 
 /*
@@ -10,18 +8,14 @@ function deferInFn(fn) {
 */
 (function () {
     "use strict";
-    // review browser support
-    if (
-        !("addEventListener" in window) ||
-        !document.documentElement.classList
-    ) {
-        return;
-    }
 
-    var navPanelSelector = "ontario-header-navigation",
-        isReadyClass = "ontario-header-navigation--is-ready",
-        isActiveClass = "ontario-header-navigation--open",
-        mobileMenuActiveClass = "ontario-mobile-navigation--open";
+    if (!("addEventListener" in window) || !document.documentElement.classList)
+        return;
+
+    var navPanelSelector = "ontario-navigation",
+        isReadyClass = "ontario-navigation--is-ready",
+        isActiveClass = "ontario-navigation--open",
+        mobileMenuActiveClass = "ontario-navigation--open";
 
     var header = document.getElementById("ontario-header"),
         nav = document.getElementById(navPanelSelector),
@@ -29,13 +23,11 @@ function deferInFn(fn) {
             "ontario-header-menu-toggler"
         ),
         closeBttnToggler = document.getElementById(
-            "ontario-header-navigation-close-toggler"
+            "ontario-header-nav-toggler"
         ),
         body = document.getElementsByTagName("body")[0];
 
-    if (!nav) {
-        return;
-    }
+    if (!nav) return;
 
     var currentTogger = null,
         currentDomEl = null;
@@ -44,8 +36,15 @@ function deferInFn(fn) {
         body.classList.add(mobileMenuActiveClass);
         header.parentNode.classList.add(isActiveClass);
         navpanel.scrollTop = 0;
-        addA11yVisiblity(navpanel);
-        focusUser({ element: navpanel, callbackOnEscape: hideNavPanel });
+
+        navpanel.classList.add("ontario-navigation--open");
+
+        addA11yVisibility(navpanel);
+
+        focusUser({
+            element: navpanel,
+            callbackOnEscape: hideNavPanel,
+        });
         deferInFn(unbindOpenBttnToggler);
 
         // bind clickables: document, closebttn - asynchronously
@@ -60,7 +59,16 @@ function deferInFn(fn) {
 
         body.classList.remove(mobileMenuActiveClass);
         header.parentNode.classList.remove(isActiveClass);
-        removeA11yVisiblity(navpanelDomEl);
+
+        var navigation = navpanelDomEl.querySelector(".ontario-navigation")
+            ? navpanelDomEl.querySelector(".ontario-navigation")
+            : navpanelDomEl;
+
+        navpanelDomEl
+            .querySelector(".ontario-navigation")
+            .classList.remove("ontario-navigation--open");
+
+        removeA11yVisibility(navigation);
 
         // unbind clickables: document, closebttn
         unbindCloseBttnToggler();
@@ -79,11 +87,12 @@ function deferInFn(fn) {
     function openMenu(e) {
         var navpanelId = e.currentTarget.getAttribute("aria-controls");
         var navpanel = document.getElementById(navpanelId);
+
         if (currentDomEl) {
             hideNavPanel(currentDomEl, false);
         }
         currentTogger = e.currentTarget;
-        currentDomEl = navpanel;
+        currentDomEl = document.querySelector(".ontario-header__container");
         showNavPanel(navpanel);
     }
 
@@ -94,15 +103,11 @@ function deferInFn(fn) {
     function onClickHandler(e) {
         var isNavPanel = e.target === currentDomEl;
         var isElementInsideNav = currentDomEl.contains(e.target);
-        if (!isNavPanel && !isElementInsideNav) {
-            hideNavPanel();
-        }
+        if (!isNavPanel && !isElementInsideNav) hideNavPanel();
     }
 
-    function onKeyboradHandler(e) {
-        if (e.key === "Escape" || e.keyCode === KEYCODE.ESCAPE) {
-            hideNavPanel();
-        }
+    function onKeyboardHandler(e) {
+        if (e.key === "Escape" || e.keyCode === KEYCODE.ESCAPE) hideNavPanel();
     }
 
     //  bind-unbind events
@@ -131,11 +136,11 @@ function deferInFn(fn) {
     }
 
     function bindDocumentKeydown() {
-        document.addEventListener("keydown", onKeyboradHandler);
+        document.addEventListener("keydown", onKeyboardHandler);
     }
 
     function unbindDocumentKeydown() {
-        document.removeEventListener("keydown", onKeyboradHandler);
+        document.removeEventListener("keydown", onKeyboardHandler);
     }
 
     function init() {
