1844dc4ef0385a2628ba560139ea041c1dd73705
Alan Harnum <alan.harnum@ontario.ca>
2022-05-11 10:52:25 -0400

    fix: deal with UTC vs local times.
    JIRA-Ref: OSI-190

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,8 +1,18 @@
 module.exports = {
-    enDateFormat: function(data) {
-      return Intl.DateTimeFormat('en-CA', { year: 'numeric', month: 'long', day: 'numeric' }).format
+
+    enDateFormatPublished: function () {
+      return Intl.DateTimeFormat('en-CA', { year: 'numeric', month: 'long', day: 'numeric' , 'timeZone': 'UTC'}).format;
     },
-    frDateFormat: function(data) {
-      return Intl.DateTimeFormat('fr-CA', { year: 'numeric', month: 'long', day: 'numeric' }).format
+
+    enDateFormatUpdated: function () {
+      return Intl.DateTimeFormat('en-CA', { year: 'numeric', month: 'long', day: 'numeric' , 'timeZone': 'America/Toronto'}).format;
+    },
+
+    frDateFormatPublished: function () {
+      return Intl.DateTimeFormat('fr-CA', { year: 'numeric', month: 'long', day: 'numeric' , 'timeZone': 'UTC'}).format;
+    },
+
+    frDateFormatUpdated: function () {
+      return Intl.DateTimeFormat('fr-CA', { year: 'numeric', month: 'long', day: 'numeric' , 'timeZone': 'America/Toronto'}).format;
     }
   }
