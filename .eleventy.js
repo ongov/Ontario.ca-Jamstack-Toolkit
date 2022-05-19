641c4542a1f9b85db7ed18fe561d2c7b580c5508
Matt St Jean <matt.st.jean@ontario.ca>
2022-05-19 14:45:48 -0400

    refactor: add nesting of locale strings and markdown filter

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,18 @@ const localeStrings = require("./src/_data/localeStrings.json")
 
 const fs = require('fs')
 const matter = require('gray-matter')
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({"src/assets" : "jamstack-toolkit/assets"})
+    eleventyConfig.addPassthroughCopy({"src/assets" : "jamstack-toolkit/assets"});
+
+    const md = new markdownIt({
+      html: true,
+    });
+
+    eleventyConfig.addFilter("markdown", (content) => {
+      return md.render(content);
+    });
 
     eleventyConfig.addFilter("localeString", function(key) {
       // Solution for accessing page front matter from https://stackoverflow.com/a/67746326
@@ -13,7 +22,19 @@ module.exports = function(eleventyConfig) {
       var str = fs.readFileSync(page.inputPath, 'utf8')
       var data = matter(str).data;
       var lang = data.lang || "en";
-      return `${localeStrings[key][lang]}`;
+
+      if (key.includes(".")) {
+        var keyArr = key.split(".");
+        var localeString = localeStrings[keyArr.shift()];
+        if (keyArr.length > 0) {
+          keyArr.forEach( (key) => {
+            localeString = localeString[key];
+          });
+        }
+      } else {
+        var localeString = localeStrings[key]
+      }
+      return `${localeString[lang]}`;
     });
 
     // Return your Object options:
