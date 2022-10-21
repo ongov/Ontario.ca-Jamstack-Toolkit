16b45bfb24fcb0818bb2e7f29fca05e581d2a5be
Alan Harnum <alan.harnum@ontario.ca>
2022-10-21 11:53:59 -0400

    release: public 0.18.0
    JIRA-Ref: release/0.18.0

---
+++ src/assets/vendor/ontario-design-system/scripts/ontario-search.js
@@ -0,0 +1,5 @@
+function doSearch() {
+    /* write your search code inside this function */
+    var searchInputField = document.getElementById("ontario-search-input-field");
+    console.log("search input field:", searchInputField.value);
+}
